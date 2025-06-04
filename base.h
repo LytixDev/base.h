a59c93fec252ab1a9cc4c1739c08239c8a62b011
Nicolai Brand <nicolaibrand2002@gmail.com>
2025-06-04 17:57:21 +0200

    useful macros

--- base.h
+++ base.h
@@ -85,6 +85,19 @@ typedef double f64;
 #define S64_MIN (-0x7fffffffffffffffll - 1)
 #define S64_MAX 0x7fffffffffffffffll
 
+#define ASSERT_NOT_REACHED assert(0 && "panic: unreachable code reached")
+
+#define VA_NUMBER_OF_ARGS(...) (sizeof((int[]){ __VA_ARGS__ }) / sizeof(int))
+
+#define ARRAY_LENGTH(arr) (size_t)(sizeof(arr) / sizeof((arr)[0]))
+
+#define IS_BETWEEN(x, lower, upper) (((lower) <= (x)) && ((x) <= (upper)))
+
+// TODO: we assume NULL is 0x0 in many places. This holds true for every compiler I've ever seen,
+//       but its not guaranteed. So we should give an error if we detect it is in fact not 0.
+//       Same is true for page size. sac_single assumes a page size of 4096. Usually true, not
+//       guaranteed.
+
 /* 
  * Originally from sac_single.h 
  */
